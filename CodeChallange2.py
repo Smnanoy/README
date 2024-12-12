3369539f17763817397005352f6d9b648e87ffa0
Smnanoy <nanoysteppenaverde@gmail.com>
2024-12-12 23:33:16 +0800

    Add files via upload

---
+++ CodeChallange2.py
@@ -0,0 +1,3 @@
+name = input("What is your name")
+print("Hi, +name")
+print("\n\t\t\t\t\t\t\t\t\t\t\t        *\n\t\t\t\t\t\t\t\t\t\t\t     * * *\n\t\t\t\t\t\t\t\t\t\t\t   * * * * *\n\t\t\t\t\t\t\t\t\t\t\t *Hi +name*\n\t\t\t\t\t\t\t\t\t\t\t   * * * * *\n\t\t\t\t\t\t\t\t\t\t\t     * * *\n\t\t\t\t\t\t\t\t\t\t\t        *")
