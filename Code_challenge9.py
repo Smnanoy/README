37bffa1cd5483b498d305b27646470830f645eb4
Smnanoy <nanoysteppenaverde@gmail.com>
2024-12-12 23:35:43 +0800

    Add files via upload

---
+++ Code_challenge9.py
@@ -0,0 +1,12 @@
+b = 7
+
+for j in range(b, 0, -1):
+    for k in range(b - j):
+            print(" ", end="")
+            
+    for n in range(j):
+            print("*", end="")
+    print()          
+            
+            
+        
